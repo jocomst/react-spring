bad207ffa49390dc5de37565d84aaa0b8c90126e
John <johncomstock9716@gmail.com>
2022-06-02 15:42:56 -0400

    Initial commit

---
+++ react-spring/src/main/java/com/example/reactspring/WebSocketConfiguration.java
@@ -0,0 +1,26 @@
+package com.example.reactspring;
+
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+@Component
+@EnableWebSocketMessageBroker // <1>
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer { // <2>
+
+    static final String MESSAGE_PREFIX = "/topic"; // <3>
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) { // <4>
+        registry.addEndpoint("/payroll").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) { // <5>
+        registry.enableSimpleBroker(MESSAGE_PREFIX);
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
+// end::code[]
